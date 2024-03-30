3a2d1275c3272b50f3b7dc7b7ca71e0a0e54e729
Paul Tisserant <paul2.tisserant@gmx.fr>
2024-03-30 22:24:37 +0100

    annulation avec remboursement d'une reservation

--- src/Controller/ListeReservationsController.php
+++ src/Controller/ListeReservationsController.php
@@ -8,6 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Offre;
 
 
 
@@ -22,8 +23,43 @@ class ListeReservationsController extends AbstractController
         // Récupérer toutes les réservations de l'utilisateur
         $reservations = $entityManager->getRepository(Reservation::class)->findBy(['id_user' => $user]);
 
-        return $this->render('liste_reservations/index.html.twig', [
-            'reservations' => $reservations,
-        ]);
+        
+            // Créer un tableau pour stocker les détails de chaque réservation (réservation + offre)
+            $reservationsDetails = [];
+    
+            // Pour chaque réservation, obtenir l'offre associée
+            foreach ($reservations as $reservation) {
+                $offre = $entityManager->getRepository(Offre::class)->find($reservation->getIdOffre());
+                
+                // Ajouter les détails de la réservation et de l'offre au tableau
+                $reservationsDetails[] = [
+                    'reservation' => $reservation,
+                    'offre' => $offre,
+                ];
+            }
+    
+            return $this->render('liste_reservations/index.html.twig', [
+                'reservationsDetails' => $reservationsDetails,
+            ]);
+    }
+
+    #[Route('/liste/reservations/annulation{id}', name: 'app_annuler_reservation')]
+    public function annulerReservation(EntityManagerInterface $entityManager, $id): Response
+    {
+        // Récupérer la réservation à annuler
+        $reservation = $entityManager->getRepository(Reservation::class)->find($id);
+
+        // rembourser le client
+        $offre = $entityManager->getRepository(Offre::class)->find($reservation->getIdOffre());
+        $user = $this->getUser();
+        $user->setPorteMonnaie($user->getPorteMonnaie() + $offre->getPrix());
+        $entityManager->persist($user);
+
+        // Supprimer la réservation
+        $entityManager->remove($reservation);
+        $entityManager->flush();
+
+        // Rediriger l'utilisateur vers la liste des réservations
+        return $this->redirectToRoute('app_liste_reservations');
     }
 }
